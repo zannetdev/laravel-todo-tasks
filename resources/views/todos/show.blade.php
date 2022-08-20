918a5e811d80712332dc4b9efd52fbcb40ad433d
zannetdev <randellcode@outlook.es>
2022-08-20 02:19:56 -0500

    My First App :D In Laravel

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 @section('content')
 <div class="container w-25 border p-4">
-    <form action="{{ route('todos') }}" method="POST">
+    <form action="{{ route('todos-update', ['id' => $todo->id]) }}" method="POST">
         @csrf
-
+        @method('PATCH')
         @if (session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -23,25 +23,9 @@
 
         <div class="form-group">
             <label for="">Titulo de la Tarea</label>
-            <input type="text" id="title" name="title" class="form-control" placeholder="Titulo" aria-describedby="helpId">
+            <input value="{{ $todo->title }}" type="text" id="title" name="title" class="form-control" placeholder="Titulo" aria-describedby="helpId">
         </div>
-        <button class="btn btn-secondary btn-rounded btn-block">Guardar</button>
+        <button class="btn btn-secondary btn-rounded btn-block">Actualizar</button>
     </form>
-    <div class="">
-        @foreach ($todos as $todo )
-        <div class="row py-1">
-            <div class="col-md-9 d-flex align-items-center">
-                <a href="{{ route('todos-edit', ['id' => $todo->id]) }}">{{ $todo->title }}</a>
-            </div>
-            <div class="col-md-3 d-flex justify-content-end">
-                <form action="{{ route('todos-destroy', ['id' => $todo->id]) }}" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-outline-danger btn-sm">Eliminar</button>
-                </form>
-            </div>
-        </div>
-        @endforeach
-    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\TodosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/todos', function () {
-    return view('todos.index');
-});
+Route::get('/tareas', [TodosController::class, 'index'])->name('todos');
+
+Route::post('/tareas', [TodosController::class, 'store'])->name('todos');
+
+Route::get('/tareas/{id}', [TodosController::class, 'show'])->name('todos-edit');
+
+Route::patch('/tareas/{id}', [TodosController::class, 'update'])->name('todos-update');
+
+Route::delete('/tareas/{id}', [TodosController::class, 'destroy'])->name('todos-destroy');
+
+
+Route::resource('categories', CategoriesController::class);
